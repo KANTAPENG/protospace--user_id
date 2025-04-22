d5aa935426210d672b869669aedeecc389f82ac5
KANTAPENG <kanta112233jw@gmail.com>
2025-04-22 12:13:42 +0900

    commit

--- db/migrate/20250410050038_create_comments.rb
+++ db/migrate/20250410050038_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.integer :user_id
       t.integer :prototype_id
       t.text :text
       t.references :user,            null: false, foreign_key: true
